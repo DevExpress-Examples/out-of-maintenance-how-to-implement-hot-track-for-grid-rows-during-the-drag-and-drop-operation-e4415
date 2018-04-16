2f07a52159bf025b4d64fc47c25567c5c66ab1ad
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿// Developer Express Code Central Example:
+// How to implement hot-track for grid rows
+// 
+// This example demonstrates how to implement the hot-track functionality in
+// TableView.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3859
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Grid;
+using System.Diagnostics;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        public static readonly DependencyProperty HotTrackedRowHandleProperty = DependencyProperty.Register("HotTrackedRowHandle", typeof(int), typeof(MainWindow), new UIPropertyMetadata(-10000));
+
+        public int HotTrackedRowHandle {
+            get {
+                return (int)GetValue(HotTrackedRowHandleProperty);
+            }
+            set {
+                SetValue(HotTrackedRowHandleProperty, value);
+            }
+        }       
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        void MainWindow_PreviewKeyDown(object sender, KeyEventArgs e) {
+           if(e.Key == Key.Escape){
+               HotTrackedRowHandle = GridControl.InvalidRowHandle;
+               return;
+           }
+        }
+
+        private void GridDragDropManager_DragOver(object sender, DevExpress.Xpf.Grid.DragDrop.GridDragOverEventArgs e) {
+            if (e.HitInfo.InRow) {
+                HotTrackedRowHandle = e.HitInfo.RowHandle;        
+            }
+        }
+
+        private void GridDragDropManager_Dropped(object sender, DevExpress.Xpf.Grid.DragDrop.GridDroppedEventArgs e) {
+            HotTrackedRowHandle = GridControl.InvalidRowHandle;
+        }
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,54 @@
+﻿' Developer Express Code Central Example:
+' How to implement hot-track for grid rows
+' 
+' This example demonstrates how to implement the hot-track functionality in
+' TableView.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E3859
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace WpfApplication
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Public Shared ReadOnly HotTrackedRowHandleProperty As DependencyProperty = DependencyProperty.Register("HotTrackedRowHandle", GetType(Integer), GetType(MainWindow), New UIPropertyMetadata(-10000))
+
+		Public Property HotTrackedRowHandle() As Integer
+			Get
+				Return CInt(Fix(GetValue(HotTrackedRowHandleProperty)))
+			End Get
+			Set(ByVal value As Integer)
+				SetValue(HotTrackedRowHandleProperty, value)
+			End Set
+		End Property
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub GridDragDropManager_DragOver(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.DragDrop.GridDragOverEventArgs)
+			If e.HitInfo.InRow Then
+
+				HotTrackedRowHandle = e.HitInfo.RowHandle
+			End If
+		End Sub
+	End Class
+End Namespace
